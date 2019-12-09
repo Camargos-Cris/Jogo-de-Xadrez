20350a4b4289f7856560b5ea7b8e337ec7e162a3
Cristiano Camargos <cristiano.ccamargos@gmail.com>
2019-12-09 20:39:02 -0300

    Titulo

---
+++ Xadrez/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Xadrez
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Xadrez");
+            Console.WriteLine("Titulo");
+        }
+    }
+}
